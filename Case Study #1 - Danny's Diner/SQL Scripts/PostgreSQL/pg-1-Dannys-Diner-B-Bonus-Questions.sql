cbf216257728dffe5e296f3147ed5b39f5bc541e
khanhtkle <137462514+khanhtkle@users.noreply.github.com>
2023-06-26 13:26:39 +0700

    Upload PostgreSQL Scripts

---
+++ Case Study #1 - Danny's Diner/SQL Scripts/PostgreSQL/pg-1-Dannys-Diner-B-Bonus-Questions.sql
@@ -0,0 +1,43 @@
+	-------------------------------------
+-- B. Bonus Questions --
+-------------------------------------
+-- 1. Join All The Things
+SELECT	
+	sa.customer_id, 
+	order_date, 
+	product_name, 
+	price,
+	CASE WHEN order_date < join_date OR join_date IS NULL 
+		 THEN 'N' ELSE 'Y' END AS member
+FROM
+	dannys_diner.sales AS sa
+			JOIN 
+	dannys_diner.menu AS mn ON mn.product_id = sa.product_id
+			LEFT JOIN
+	dannys_diner.members AS mb ON mb.customer_id = sa.customer_id
+ORDER BY 
+	1, 2, 3;
+
+
+-- 	2. Rank All The Things
+
+SELECT 
+	sa.customer_id, 
+	order_date, 
+	product_name, 
+	price, 
+	CASE WHEN order_date < join_date OR join_date IS NULL 
+		 THEN 'N' ELSE 'Y' END AS member, 
+	CASE WHEN order_date < join_date OR join_date IS NULL 
+		 THEN NULL ELSE DENSE_RANK() OVER (PARTITION BY (
+									CASE WHEN order_date < join_date OR join_date IS NULL 
+			 							 THEN NULL ELSE sa.customer_id END
+													) ORDER BY order_date) END AS ranking 
+FROM
+	dannys_diner.sales AS sa
+			JOIN	
+	dannys_diner.menu AS mn ON mn.product_id = sa.product_id
+			LEFT JOIN
+	dannys_diner.members AS mb ON mb.customer_id = sa.customer_id
+ORDER BY 
+	1, 2, 3;
