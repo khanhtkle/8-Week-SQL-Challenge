b889c3752caf2dcda5f2a0e21b8533ecb1b10898
khanhtkle <137462514+khanhtkle@users.noreply.github.com>
2023-07-02 20:26:16 +0700

    Upload T-SQL Scripts

--- Case Study #1 - Danny's Diner/SQL Scripts/T-SQL/tsql-1-Dannys-Diner-B-Bonus-Questions.sql
+++ Case Study #1 - Danny's Diner/SQL Scripts/T-SQL/tsql-1-Dannys-Diner-B-Bonus-Questions.sql
@@ -3,22 +3,21 @@
 -------------------------------------
 -- 	1. Join All The Things - Create a table that has these columns: customer_id, order_date, product_name, price, member (Y/N).
 
-SELECT	
-	sa.customer_id, 
-	order_date, 
-	product_name, 
-	price,
-	CASE WHEN order_date < join_date OR join_date IS NULL 
-		 THEN 'N' ELSE 'Y' END AS member
-FROM
-	dannys_diner.dbo.sales AS sa
-			JOIN 
-	dannys_diner.dbo.menu AS mn ON mn.product_id = sa.product_id
-			LEFT JOIN
-	dannys_diner.dbo.members AS mb ON mb.customer_id = sa.customer_id
-ORDER BY 
-	sa.customer_id, order_date, product_name;
-
+SELECT sa.customer_id,
+       order_date,
+       product_name,
+       price,
+       CASE
+           WHEN order_date < join_date
+                OR join_date IS NULL THEN 'N'
+           ELSE 'Y'
+       END AS member
+FROM dannys_diner.dbo.sales AS sa
+JOIN dannys_diner.dbo.menu AS mn ON mn.product_id = sa.product_id
+LEFT JOIN dannys_diner.dbo.members AS mb ON mb.customer_id = sa.customer_id
+ORDER BY sa.customer_id,
+         order_date,
+         product_name;
 
 -- 	2. Rank All The Things - Based on the table above, add one column: ranking.
 
