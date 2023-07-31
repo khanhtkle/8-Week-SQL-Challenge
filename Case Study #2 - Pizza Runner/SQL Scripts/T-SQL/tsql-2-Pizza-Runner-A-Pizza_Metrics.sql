43ddd541ba6551e0d3b3a55411f4848ab02849ff
khanhtkle <137462514+khanhtkle@users.noreply.github.com>
2023-07-31 11:27:06 +0700

    Upload T-SQL Scripts

--- Case Study #2 - Pizza Runner/SQL Scripts/T-SQL/tsql-2-Pizza-Runner-A-Pizza_Metrics.sql
+++ Case Study #2 - Pizza Runner/SQL Scripts/T-SQL/tsql-2-Pizza-Runner-A-Pizza_Metrics.sql
@@ -8,9 +8,6 @@ FROM pizza_runner.dbo.cleaned_customer_orders;
 
 --	2. How many unique customer orders were made?
 
-SELECT COUNT(DISTINCT customer_id) AS unqiue_customer_count
-FROM pizza_runner.dbo.cleaned_customer_orders;
-
 WITH unique_orders_cte AS
   (SELECT DISTINCT pizza_id,
           exclusions,
@@ -30,14 +27,13 @@ GROUP BY runner_id;
 --	4. How many of each type of pizza was delivered?
 
 SELECT pizza_id,
-       COUNT(ro.order_id) AS delivered_pizza_count
-FROM pizza_runner.dbo.cleaned_customer_orders AS co
-JOIN pizza_runner.dbo.cleaned_runner_orders AS ro ON ro.order_id = co.order_id
-WHERE co.cancellation IS NULL
+       COUNT(order_id) AS delivered_pizza_count
+FROM pizza_runner.dbo.cleaned_customer_orders
+WHERE cancellation IS NULL
 GROUP BY pizza_id;
 
 --	5. How many Vegetarian and Meatlovers were ordered by each customer?
---	Convert the data type of `pizza_name` from TEXT to VARCHAR(10) to enable the use of equal-to operator for comparisons involving the `pizza_name` column and string values.
+--	Convert the data type of `pizza_name` from TEXT to VARCHAR(10) for broader compatibility with table-valued functions and to prevent potential errors.
 
 ALTER TABLE pizza_runner.dbo.pizza_names
 ALTER COLUMN pizza_name VARCHAR(10);
@@ -47,24 +43,22 @@ SELECT customer_id,
        SUM(CASE
                WHEN pizza_name = 'Meatlovers' THEN 1
                ELSE 0
-           END) AS delivered_meatlovers_count,
+           END) AS ordered_meatlovers_count,
        SUM(CASE
                WHEN pizza_name = 'Vegetarian' THEN 1
                ELSE 0
-           END) AS delivered_vegetarian_count
+           END) AS ordered_vegetarian_count
 FROM pizza_runner.dbo.cleaned_customer_orders AS ro
 JOIN pizza_runner.dbo.pizza_names AS pn ON pn.pizza_id= ro.pizza_id
 GROUP BY customer_id;
 
 --	6. What was the maximum number of pizzas delivered in a single order?
 
-SELECT TOP 1 ro.order_id,
-       COUNT(pizza_id) AS delivered_pizza_count
-FROM pizza_runner.dbo.cleaned_customer_orders AS co
-JOIN pizza_runner.dbo.cleaned_runner_orders AS ro ON ro.order_id = co.order_id
-WHERE co.cancellation IS NULL
-GROUP BY ro.order_id
-ORDER BY delivered_pizza_count DESC;
+SELECT TOP 1 COUNT(pizza_id) AS max_delivered_pizza_in_a_single_order
+FROM pizza_runner.dbo.cleaned_customer_orders
+WHERE cancellation IS NULL
+GROUP BY order_id
+ORDER BY max_delivered_pizza_in_a_single_order DESC;
 
 --	7. For each customer, how many delivered pizzas had at least 1 change and how many had no changes?
 
@@ -78,9 +72,8 @@ SELECT SUM(CASE
                     AND extras IS NULL THEN 1
                ELSE 0
            END) AS delivered_pizza_with_no_changes_count
-FROM pizza_runner.dbo.cleaned_customer_orders AS co
-JOIN pizza_runner.dbo.cleaned_runner_orders AS ro ON ro.order_id = co.order_id
-WHERE co.cancellation IS NULL;
+FROM pizza_runner.dbo.cleaned_customer_orders
+WHERE cancellation IS NULL;
 
 --	8. How many pizzas were delivered that had both exclusions and extras?
 
@@ -89,9 +82,8 @@ SELECT SUM(CASE
                     AND extras IS NOT NULL THEN 1
                ELSE 0
            END) AS delivered_pizza_with_exclusions_and_extras_count
-FROM pizza_runner.dbo.cleaned_customer_orders AS co
-JOIN pizza_runner.dbo.cleaned_runner_orders AS ro ON ro.order_id = co.order_id
-WHERE co.cancellation IS NULL;
+FROM pizza_runner.dbo.cleaned_customer_orders
+WHERE cancellation IS NULL;
 
 --	9. What was the total volume of pizzas ordered for each hour of the day?
 
@@ -103,8 +95,9 @@ ORDER BY DATEPART(hh, order_time);
 
 --	10. What was the volume of orders for each day of the week?
 
-SELECT	DATENAME(dw, order_time) AS day_of_week,
-		COUNT(order_id) AS ordered_pizza_count
+SELECT DATENAME(dw, order_time) AS day_of_week,
+	     COUNT(order_id) AS ordered_pizza_count
 FROM pizza_runner.dbo.cleaned_customer_orders
-GROUP BY DATENAME(dw, order_time), DATEPART(dw, order_time)
+GROUP BY DATENAME(dw, order_time),
+         DATEPART(dw, order_time)
 ORDER BY DATEPART(dw, order_time);
