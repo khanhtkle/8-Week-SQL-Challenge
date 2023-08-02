692ab3e644d4bdd41d5424b356bc101d78fcbb4d
khanhtkle <137462514+khanhtkle@users.noreply.github.com>
2023-08-02 11:31:55 +0700

    Upload T-SQL Scripts

--- Case Study #2 - Pizza Runner/SQL Scripts/T-SQL/tsql-2-Pizza-Runner-E-Bonus-Questions.sql
+++ Case Study #2 - Pizza Runner/SQL Scripts/T-SQL/tsql-2-Pizza-Runner-E-Bonus-Questions.sql
@@ -1,46 +1,26 @@
 ---------------------------------------
 -- E. Bonus Questions --
 ---------------------------------------
--- If Danny wants to expand his range of pizzas - how would this impact the existing data design? Write an INSERT statement to demonstrate what would happen if a new Supreme pizza with all the toppings was added to the Pizza Runner menu?
-
-DROP TABLE IF EXISTS pizza_runner.dbo.pizza_names;
-CREATE TABLE pizza_runner.dbo.pizza_names (
-  "pizza_id" INTEGER,
-  "pizza_name" TEXT
-);
+--     If Danny wants to expand his range of pizzas - how would this impact the existing data design? Write an INSERT statement to demonstrate what would happen if a new Supreme pizza with all the toppings was added to the Pizza Runner menu?
+--     This is how `pizza_name` table changes when adding Supreme pizza to the menu.
 
 INSERT INTO pizza_runner.dbo.pizza_names
   ("pizza_id", "pizza_name")
-VALUES
-  (1, 'Meatlovers'),
-  (2, 'Vegetarian');
-
-
-DROP TABLE IF EXISTS pizza_runner.dbo.pizza_recipes;
-CREATE TABLE pizza_runner.dbo.pizza_recipes (
-  "pizza_id" INTEGER,
-  "toppings" TEXT
-);
-
-INSERT INTO pizza_runner.dbo.pizza_recipes
-  ("pizza_id", "toppings")
-VALUES
-  (1, '1, 2, 3, 4, 5, 6, 8, 10'),
-  (2, '4, 6, 7, 9, 11, 12');
-
-INSERT INTO pizza_runner.dbo.pizza_names
-  ("pizza_id", "pizza_name")
-VALUES 
-  (3, 'Supreme');
+SELECT 3, 'Supreme'
+WHERE NOT EXISTS (SELECT 1
+                  FROM pizza_runner.dbo.pizza_names
+                  WHERE pizza_id = 3);
 
 SELECT * FROM pizza_runner.dbo.pizza_names;
 
-ALTER TABLE pizza_runner.dbo.pizza_recipes
-ALTER COLUMN toppings VARCHAR(43);
-GO
-
-INSERT INTO pizza_runner.dbo.pizza_recipes(pizza_id, toppings)
-VALUES (3, (SELECT STRING_AGG(topping_id, ', ') WITHIN GROUP (ORDER BY topping_id) FROM pizza_runner.dbo.pizza_toppings))
+--     This is how `pizza_recipes` table changes when adding Supreme pizza to the menu.
+    
+INSERT INTO pizza_runner.dbo.pizza_recipes
+  ("pizza_id", "toppings")
+SELECT 3, (SELECT STRING_AGG(topping_id, ', ') WITHIN GROUP (ORDER BY topping_id)
+           FROM pizza_runner.dbo.pizza_toppings)
+WHERE NOT EXISTS (SELECT 1
+                  FROM pizza_runner.dbo.pizza_recipes
+                  WHERE pizza_id = 3);
 
 SELECT * FROM pizza_runner.dbo.pizza_recipes;
-
