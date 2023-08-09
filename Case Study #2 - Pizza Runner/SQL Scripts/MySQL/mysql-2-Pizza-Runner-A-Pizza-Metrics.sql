a2abc3defe058a267b1a016d8c61f980746d7923
khanhtkle <137462514+khanhtkle@users.noreply.github.com>
2023-08-09 09:09:18 +0700

    Upload MySQL Scripts

--- Case Study #2 - Pizza Runner/SQL Scripts/MySQL/mysql-2-Pizza-Runner-A-Pizza-Metrics.sql
+++ Case Study #2 - Pizza Runner/SQL Scripts/MySQL/mysql-2-Pizza-Runner-A-Pizza-Metrics.sql
@@ -1,59 +1,99 @@
--------------------------------------------
--- A. Data Cleaning: Pizza Metrics --
--------------------------------------------
--- 	Create a table `cleaned_runner_orders` from `runner_orders` table:
--- 		Convert all blank '' and 'null' text values in `pickup_time`, `duration` and `cancellation` into NULL values.
--- 		Convert the data type of `pickup_time` from VARCHAR(19) to DATETIME.
--- 		Remove the "km" suffix and convert the data type of `distance` from VARCHAR(7) to FLOAT.
--- 		Remove the suffixes 'mins', 'minute', 'minutes' and convert the data type of `distance` from VARCHAR(10) to UNSIGNED.
+---------------------------------------
+-- A. Pizza Metrics --
+---------------------------------------
+-- 	1. How many pizzas were ordered?
 
-DROP TABLE IF EXISTS pizza_runner.cleaned_runner_orders;
-CREATE TABLE pizza_runner.cleaned_runner_orders AS
-  (SELECT order_id,
-          runner_id,
-          CASE
-              WHEN pickup_time = 'null' THEN NULL
-              ELSE CAST(pickup_time AS DATETIME)
-          END AS pickup_time,
-          CASE
-              WHEN distance = 'null' THEN NULL
-              ELSE CAST(TRIM('km' FROM distance) AS FLOAT)
-          END AS distance,
-          CASE
-              WHEN duration = 'null' THEN NULL
-              ELSE CAST(SUBSTRING(duration, 1, 2) AS UNSIGNED)
-          END AS duration,
-          CASE
-              WHEN cancellation in ('null', '') THEN NULL
-              ELSE cancellation
-          END AS cancellation
-   FROM pizza_runner.runner_orders);
-
-SELECT *
-FROM pizza_runner.cleaned_runner_orders;
-
--- 	Create a new table `cleaned_customer_orders` from `customer_orders` table:
--- 		Convert all blank '' and 'null' text values in `exclusions` and `extras` into NULL values.
--- 		Convert the data type of `order_time` from VARCHAR(19) to DATETIME.
--- 		Append `cancellation` from `cleaned_runner_orders` table.
-
-DROP TABLE IF EXISTS pizza_runner.cleaned_customer_orders;
-CREATE TABLE pizza_runner.cleaned_customer_orders AS
-  (SELECT co.order_id,
-          customer_id,
-          pizza_id,
-          CASE
-              WHEN exclusions IN ('null', '') THEN NULL
-              ELSE exclusions
-          END AS exclusions,
-          CASE
-              WHEN extras IN ('null', 'NULL', '') THEN NULL
-              ELSE extras
-          END AS extras,
-          CAST(order_time AS DATETIME) AS order_time,
-          cancellation
-   FROM pizza_runner.customer_orders AS co
-   JOIN pizza_runner.cleaned_runner_orders AS ro ON ro.order_id = co.order_id);
-
-SELECT *
+SELECT COUNT(order_id) AS pizza_count
 FROM pizza_runner.cleaned_customer_orders;
+
+-- 	2. How many unique customer orders were made?
+
+WITH unique_orders_cte AS
+  (SELECT DISTINCT pizza_id,
+          exclusions,
+          extras
+   FROM pizza_runner.cleaned_customer_orders)
+SELECT COUNT(*) AS unique_order_count
+FROM unique_orders_cte;
+
+-- 	3. How many successful orders were delivered by each runner?
+
+SELECT runner_id,
+       COUNT(order_id) AS delivered_order_count
+FROM pizza_runner.cleaned_runner_orders
+WHERE cancellation IS NULL
+GROUP BY 1;
+
+-- 	4. How many of each type of pizza was delivered?
+
+SELECT pizza_id,
+       COUNT(order_id) AS delivered_order_count
+FROM pizza_runner.cleaned_customer_orders
+WHERE cancellation IS NULL
+GROUP BY 1;
+
+-- 	5. How many Vegetarian and Meatlovers were ordered by each customer?
+
+SELECT customer_id,
+       SUM(CASE
+               WHEN pizza_name = 'Meatlovers' THEN 1
+               ELSE 0
+           END) AS ordered_meatlovers_count,
+       SUM(CASE
+               WHEN pizza_name = 'Vegetarian' THEN 1
+               ELSE 0
+           END) AS ordered_vegetarian_count
+FROM pizza_runner.cleaned_customer_orders AS ro
+JOIN pizza_runner.pizza_names AS pn ON pn.pizza_id = ro.pizza_id
+GROUP BY 1;
+
+-- 	6. What was the maximum number of pizzas delivered in a single order?
+
+SELECT COUNT(pizza_id) AS max_delivered_pizza_in_a_single_order
+FROM pizza_runner.cleaned_customer_orders
+WHERE cancellation IS NULL
+GROUP BY order_id
+ORDER BY 1 DESC
+LIMIT 1;
+
+-- 	7. For each customer, how many delivered pizzas had at least 1 change and how many had no changes?
+
+SELECT SUM(CASE
+               WHEN exclusions IS NOT NULL
+                    OR extras IS NOT NULL THEN 1
+               ELSE 0
+           END) AS delivered_pizza_with_at_least_1_change_count,
+       SUM(CASE
+               WHEN exclusions IS NULL
+                    AND extras IS NULL THEN 1
+               ELSE 0
+           END) AS delivered_pizza_with_no_changes_count
+FROM pizza_runner.cleaned_customer_orders AS co
+JOIN pizza_runner.cleaned_runner_orders AS ro ON ro.order_id = co.order_id
+WHERE co.cancellation IS NULL;
+
+-- 	8. How many pizzas were delivered that had both exclusions and extras?
+
+SELECT SUM(CASE
+               WHEN exclusions IS NOT NULL
+                    AND extras IS NOT NULL THEN 1
+               ELSE 0
+           END) AS delivered_pizza_with_exclusions_and_extras_count
+FROM pizza_runner.cleaned_customer_orders
+WHERE cancellation IS NULL;
+
+-- 	9. What was the total volume of pizzas ordered for each hour of the day?
+
+SELECT HOUR(order_time) AS hour_of_day,
+       COUNT(order_id) AS ordered_pizza_count
+FROM pizza_runner.cleaned_customer_orders
+GROUP BY 1
+ORDER BY 1;
+
+-- 	10. What was the volume of orders for each day of the week?
+
+SELECT	DAYNAME(order_time) AS day_of_week,
+        COUNT(order_id) AS ordered_pizza_count
+FROM pizza_runner.cleaned_customer_orders
+GROUP BY 1
+ORDER BY 2;
