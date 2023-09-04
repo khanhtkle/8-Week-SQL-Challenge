908cd002bad0c04e3d4c6013e18a6f9a9b500796
khanhtkle <137462514+khanhtkle@users.noreply.github.com>
2023-09-04 14:00:55 +0700

    Upload T-SQL Scripts

--- Case Study #3 - Foodie-Fi/SQL Scripts/T-SQL/tsql-3-Foodie-Fi-A-Customer-Journey.sql
+++ Case Study #3 - Foodie-Fi/SQL Scripts/T-SQL/tsql-3-Foodie-Fi-A-Customer-Journey.sql
@@ -1,11 +1,11 @@
 -------------------------------------
 -- A. Customer Journey --
 -------------------------------------
---	Based off the 8 sample customers provided in the sample from the `subscriptions` table, write a brief description about each customer’s onboarding journey. Try to keep it as short as possible - you may also want to run some sort of join to make your explanations a bit easier!
+--	Based off the 8 sample customers provided in the sample from the `subscriptions` table, write a brief description about each customerâ€™s onboarding journey. Try to keep it as short as possible - you may also want to run some sort of join to make your explanations a bit easier!
 
 SELECT su.*,
-	   plan_name,
-	   price 
+       plan_name,
+       price
 FROM foodie_fi.dbo.subscriptions AS su
 JOIN foodie_fi.dbo.plans AS pl ON pl.plan_id = su.plan_id
 WHERE customer_id IN (1, 2, 11, 13, 15, 16, 18, 19)
