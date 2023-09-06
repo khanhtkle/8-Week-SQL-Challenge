18ca33fe86f73ce9892fe3de61a2ad80e0dce617
khanhtkle <137462514+khanhtkle@users.noreply.github.com>
2023-09-06 11:31:47 +0700

    Upload MySQL Scripts

--- Case Study #3 - Foodie-Fi/SQL Scripts/MySQL/mysql-3-Foodie-Fi-C-Challenge-Payment-Question.sql
+++ Case Study #3 - Foodie-Fi/SQL Scripts/MySQL/mysql-3-Foodie-Fi-C-Challenge-Payment-Question.sql
@@ -3,7 +3,7 @@
 ----------------------------------------
 -- 		The Foodie-Fi team wants you to create a new payments table for the year 2020 that includes amounts paid by each customer in the `subscriptions` table with the following requirements:
 -- 			- Monthly payments always occur on the same day of month as the original `start_date` of any monthly paid plan.
--- 			- Upgrades from basic to monthly or pro plans are reduced by the current paid amount in that month and start iMONTHediately.
+-- 			- Upgrades from basic to monthly or pro plans are reduced by the current paid amount in that month and start immediately.
 -- 			- Upgrades from pro monthly to pro annual are paid at the end of the current billing period and also starts at the end of the month period.
 -- 			- Once a customer churns they will no longer make payments.
 
