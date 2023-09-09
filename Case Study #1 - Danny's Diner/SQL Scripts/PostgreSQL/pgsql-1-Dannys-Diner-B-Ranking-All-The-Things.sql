4dd0de2297cbb6040614175685eac34e49ea12d9
khanhtkle <137462514+khanhtkle@users.noreply.github.com>
2023-09-09 20:24:01 +0700

    Upload PostgreSQL Scripts

--- Case Study #1 - Danny's Diner/SQL Scripts/PostgreSQL/pgsql-1-Dannys-Diner-B-Ranking-All-The-Things.sql
+++ Case Study #1 - Danny's Diner/SQL Scripts/PostgreSQL/pgsql-1-Dannys-Diner-B-Ranking-All-The-Things.sql
@@ -1,23 +1,11 @@
 -------------------------------------
--- B. Bonus Question --
+-- B. Ranking All The Things --
 -------------------------------------
--- 	1. Join All The Things - Create a table that has these columns: customer_id, order_date, product_name, price, member (Y/N).
-
-SELECT sa.customer_id,
-       order_date,
-       product_name,
-       price,
-       CASE
-           WHEN order_date < join_date
-                OR join_date IS NULL THEN 'N'
-           ELSE 'Y'
-       END AS member
-FROM dannys_diner.sales AS sa
-JOIN dannys_diner.menu AS mn ON mn.product_id = sa.product_id
-LEFT JOIN dannys_diner.members AS mb ON mb.customer_id = sa.customer_id
-ORDER BY 1, 2, 3;
-
--- 	2. Rank All The Things - Based on the table above, add one column: ranking.
+-- 	Danny also requires further information about the `ranking` of customer products, but he purposely does not need the `ranking` for non-member purchases so he expects `NULL` values for the records when customers are not yet part of the loyalty program.
+-- 	- Establish the core by including the `customer_id` and `order_date` from `sales` table.
+-- 	- Include `product_name` and `price` from 'menu' alongside their respective 'customer_id' and 'order_date'.
+-- 	- Add a column `member`, which siginifies a customer's membership status as `'Y'` when the customer's `order_date` is later than their `join_date` from `member` table, and `'N'` when it's sooner.
+-- 	- Add a `ranking` column, which assigns sequential numbers to each customer's orders, with `NULL` values expected for non-member purchases.
 
 SELECT sa.customer_id,
        order_date,
