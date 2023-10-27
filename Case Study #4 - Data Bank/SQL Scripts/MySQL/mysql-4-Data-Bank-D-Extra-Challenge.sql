3b440f37a558d5542a5a0672ad875f5afd399ccd
khanhtkle <137462514+khanhtkle@users.noreply.github.com>
2023-10-27 12:36:30 +0700

    Upload MySQL Scripts

--- Case Study #4 - Data Bank/SQL Scripts/MySQL/mysql-4-Data-Bank-D-Extra-Challenge.sql
+++ Case Study #4 - Data Bank/SQL Scripts/MySQL/mysql-4-Data-Bank-D-Extra-Challenge.sql
@@ -11,7 +11,7 @@
 DROP TABLE IF EXISTS data_bank.balance_with_daily_n_c_i_reward;
 CREATE TABLE data_bank.balance_with_daily_n_c_i_reward AS
   (SELECT customer_id, 
-		  date, 
+	  date, 
           CAST(balance * (1 + CAST(0.06 AS DECIMAL(9, 4)) / 366) AS DECIMAL(9, 4)) AS balance_with_daily_n_c_i_reward
    FROM data_bank.balance_by_day
    WHERE MONTH(date) < 5
@@ -53,22 +53,22 @@ CREATE TABLE data_bank.balance_with_daily_c_i_reward AS
              CAST(balance * (1 + CAST(0.06 AS DECIMAL(9, 4)) / 366) AS DECIMAL(9, 4)) AS balance_with_daily_c_i_reward
       FROM data_bank.first_and_last_balance_by_day AS fl
       JOIN data_bank.balance_by_day AS bd ON bd.customer_id = fl.customer_id
-										 AND bd.date = fl.first_date
+				         AND bd.date = fl.first_date
       UNION ALL 
       SELECT re.customer_id,
-			 DATE_ADD(re.date, INTERVAL 1 DAY),
+	     DATE_ADD(re.date, INTERVAL 1 DAY),
              last_date,
              bd.total_txn_amount_by_day,
              CASE
                  WHEN bd.total_txn_amount_by_day IS NULL THEN CAST(balance_with_daily_c_i_reward * (1 + CAST(0.06 AS DECIMAL(9, 4)) / 366) AS DECIMAL(9, 4))
                  ELSE CAST((balance_with_daily_c_i_reward + bd.total_txn_amount_by_day) * (1 + CAST(0.06 AS DECIMAL(9, 4)) / 366) AS DECIMAL(9, 4))
-		     END
+	     END
       FROM recursive_cte AS re
       JOIN data_bank.balance_by_day AS bd ON bd.customer_id = re.customer_id
-										 AND bd.date = DATE_ADD(re.date, INTERVAL 1 DAY)
+				         AND bd.date = DATE_ADD(re.date, INTERVAL 1 DAY)
       WHERE DATE_ADD(re.date, INTERVAL 1 DAY) <= last_date) 
    SELECT re.customer_id,
-		  re.date,
+	  re.date,
           re.total_txn_amount_by_day,
           balance_with_daily_c_i_reward
    FROM recursive_cte AS re
