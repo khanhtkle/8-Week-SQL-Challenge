f73d13c6149fbfc516b4e03dede71994bd8466ec
khanhtkle <137462514+khanhtkle@users.noreply.github.com>
2023-10-27 12:42:36 +0700

    Upload PostgreSQL Scripts

--- Case Study #4 - Data Bank/SQL Scripts/PostgreSQL/pgsql-4-Data-Bank-B-!-Data-Cleaning.sql
+++ Case Study #4 - Data Bank/SQL Scripts/PostgreSQL/pgsql-4-Data-Bank-B-!-Data-Cleaning.sql
@@ -4,8 +4,8 @@
 DROP TABLE IF EXISTS data_bank.opening_account_date;
 CREATE TABLE data_bank.opening_account_date AS
   (SELECT DISTINCT customer_id,
-                   MIN(txn_date) OVER (PARTITION BY customer_id
-                                       ORDER BY txn_date) AS opening_account_date
+          MIN(txn_date) OVER (PARTITION BY customer_id
+                              ORDER BY txn_date) AS opening_account_date
    FROM data_bank.customer_transactions
    ORDER BY 1);
 
@@ -22,17 +22,17 @@ CREATE TABLE data_bank.recursive_p_start AS
       FROM data_bank.opening_account_date
       UNION ALL 
       SELECT customer_id,
-			 opening_account_date,
+	     opening_account_date,
              (p_start + INTERVAL '1 month')::DATE
       FROM recursive_p_start_cte
       WHERE DATE_PART('month', p_start + INTERVAL '1 month') < 5) 
    SELECT *
    FROM recursive_p_start_cte
    ORDER BY 1, 3);
-   
 
 SELECT *
 FROM data_bank.recursive_p_start;
+
 
 DROP TABLE IF EXISTS data_bank.recursive_date;
 CREATE TABLE data_bank.recursive_date AS
@@ -44,7 +44,7 @@ CREATE TABLE data_bank.recursive_date AS
       FROM data_bank.recursive_p_start
       UNION ALL 
       SELECT customer_id,
-			 p_start,
+	     p_start,
              (date + INTERVAL '1 day')::DATE,
              p_end
       FROM recursive_date_cte
@@ -72,29 +72,29 @@ CREATE TABLE data_bank.customer_transactions_extended AS
              txn_type,
              txn_amount,
              SUM(CASE
-					 WHEN txn_type = 'deposit' THEN txn_amount
+		     WHEN txn_type = 'deposit' THEN txn_amount
                      ELSE -txn_amount
-				 END) OVER (PARTITION BY rd.customer_id, p_start, date
-							ORDER BY date, customer_transactions_row_number) AS d_txn_amount,
-			 SUM(CASE
-					 WHEN txn_type = 'deposit' THEN txn_amount
+		 END) OVER (PARTITION BY rd.customer_id, p_start, date
+			    ORDER BY date, customer_transactions_row_number) AS d_txn_amount,
+	     SUM(CASE
+		     WHEN txn_type = 'deposit' THEN txn_amount
                      ELSE -txn_amount
-			     END) OVER (PARTITION BY rd.customer_id
-							ORDER BY date, customer_transactions_row_number 
-                            ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS balance, 
-			 customer_transactions_row_number
+		 END) OVER (PARTITION BY rd.customer_id
+			    ORDER BY date, customer_transactions_row_number 
+             		    ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) AS balance, 
+	     customer_transactions_row_number
       FROM data_bank.recursive_date AS rd
       LEFT JOIN customer_transactions_row_number_cte AS ct ON ct.customer_id = rd.customer_id
-														  AND ct.txn_date = rd.date) 
+							  AND ct.txn_date = rd.date) 
    SELECT *,
-		  LEAD(date) OVER (PARTITION BY customer_id
-						   ORDER BY date, customer_transactions_row_number) AS next_date,
-		  ROW_NUMBER() OVER (PARTITION BY customer_id, p_start, p_end, balance
-							 ORDER BY date) AS balance_unchanged_p_day_count,
-		  p_end - p_start + 1 AS p_month_day_count,
-		  ROW_NUMBER() OVER (PARTITION BY customer_id, DATE_PART('month', date), balance
-							 ORDER BY date) AS balance_unchanged_day_count,
-		  (DATE_TRUNC('month', date) + INTERVAL '1 month - 1 day')::DATE - DATE_TRUNC('month', date)::DATE + 1 AS month_day_count
+	  LEAD(date) OVER (PARTITION BY customer_id
+			   ORDER BY date, customer_transactions_row_number) AS next_date,
+	  ROW_NUMBER() OVER (PARTITION BY customer_id, p_start, p_end, balance
+			     ORDER BY date) AS balance_unchanged_p_day_count,
+	  p_end - p_start + 1 AS p_month_day_count,
+	  ROW_NUMBER() OVER (PARTITION BY customer_id, DATE_PART('month', date), balance
+			     ORDER BY date) AS balance_unchanged_day_count,
+	  (DATE_TRUNC('month', date) + INTERVAL '1 month - 1 day')::DATE - DATE_TRUNC('month', date)::DATE + 1 AS month_day_count
    FROM balance_calculating_cte
    ORDER BY 1, 3, 9);
 
@@ -108,11 +108,11 @@ CREATE TABLE data_bank.balance_by_day AS
           p_start, 
           date, 
           p_end,
-		  d_txn_amount AS total_txn_amount_by_day,
-		  balance
+	  d_txn_amount AS total_txn_amount_by_day,
+	  balance
    FROM data_bank.customer_transactions_extended
    WHERE next_date IS NULL
-		 OR date != next_date
+	 OR date != next_date
    ORDER BY 1, 3);
 
 SELECT *
