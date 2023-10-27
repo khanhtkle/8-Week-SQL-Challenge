fbbcc7115e569666f82b1736ffa663977dadb92a
khanhtkle <137462514+khanhtkle@users.noreply.github.com>
2023-10-27 12:46:02 +0700

    Upload PostgreSQL Scripts

--- Case Study #4 - Data Bank/SQL Scripts/PostgreSQL/pgsql-4-Data-Bank-A-!-Data-Cleaning.sql
+++ Case Study #4 - Data Bank/SQL Scripts/PostgreSQL/pgsql-4-Data-Bank-A-!-Data-Cleaning.sql
@@ -9,10 +9,10 @@ CREATE TABLE data_bank.customer_nodes_aggregated AS
              node_id,
              LAG(node_id) OVER (PARTITION BY customer_id
                                 ORDER BY start_date) AS previous_node_id,
-			 LEAD(node_id) OVER (PARTITION BY customer_id
-								 ORDER BY start_date) AS next_node_id,
-			 start_date,
-			 end_date
+	     LEAD(node_id) OVER (PARTITION BY customer_id
+				 ORDER BY start_date) AS next_node_id,
+	     start_date,
+	     end_date
       FROM data_bank.customer_nodes
       WHERE DATE_PART('year', end_date) != 9999),
         node_date_filtering_2_cte AS
@@ -23,31 +23,31 @@ CREATE TABLE data_bank.customer_nodes_aggregated AS
              end_date,
              LAG(node_id) OVER (PARTITION BY customer_id
                                 ORDER BY start_date) AS previous_node_id_2,
-			 LEAD(node_id) OVER (PARTITION BY customer_id
-								 ORDER BY start_date) AS next_node_id_2,
-			 LEAD(end_date) OVER (PARTITION BY customer_id
-								  ORDER BY start_date) AS next_end_date
+	     LEAD(node_id) OVER (PARTITION BY customer_id
+				 ORDER BY start_date) AS next_node_id_2,
+	     LEAD(end_date) OVER (PARTITION BY customer_id
+				  ORDER BY start_date) AS next_end_date
       FROM node_date_filtering_1_cte
       WHERE (previous_node_id IS NULL
              OR next_node_id IS NULL
              OR node_id != previous_node_id
              OR node_id != next_node_id)) 
    SELECT customer_id,
-		  region_id,
+	  region_id,
           node_id,
           start_date,
           CASE
-			  WHEN node_id = next_node_id_2 THEN next_end_date
-			  WHEN node_id = previous_node_id_2 THEN NULL
-			  ELSE end_date
-		  END AS end_date
+	      WHEN node_id = next_node_id_2 THEN next_end_date
+	      WHEN node_id = previous_node_id_2 THEN NULL
+	      ELSE end_date
+	  END AS end_date
    FROM node_date_filtering_2_cte
    WHERE CASE
              WHEN node_id = next_node_id_2 THEN next_end_date
              WHEN node_id = previous_node_id_2 THEN NULL
              ELSE end_date
-         END IS NOT NULL)
-ORDER BY 1, 4;
+         END IS NOT NULL
+   ORDER BY 1, 4);
 
 SELECT * 
 FROM data_bank.customer_nodes_aggregated;
