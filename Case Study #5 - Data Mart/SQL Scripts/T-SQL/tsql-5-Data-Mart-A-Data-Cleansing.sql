32721253d29fb076c6e2e758e43e2b901473bb52
khanhtkle <137462514+khanhtkle@users.noreply.github.com>
2023-11-22 19:03:33 +0700

    Upload T-SQL Scripts

--- Case Study #5 - Data Mart/SQL Scripts/T-SQL/tsql-5-Data-Mart-A-Data-Cleansing.sql
+++ Case Study #5 - Data Mart/SQL Scripts/T-SQL/tsql-5-Data-Mart-A-Data-Cleansing.sql
@@ -1,7 +1,7 @@
 -------------------------------------
 -- A. Data Cleansing Steps --
 -------------------------------------
---	In a single query, perform the following operations and generate a new table the `data_mart` schema named `weekly_sales`:
+--	In a single query, perform the following operations and generate a new table the `data_mart` schema named `clean_weekly_sales`:
 --		- Convert the `week_date` to a DATE format.
 --		- Add a `week_number` as the second column for each `week_date` value, for example any value from the 1st of January to 7th of January will be 1, 8th to 14th will be 2 etc.
 --		- Add a `month_number` with the calendar month for each `week_date` value as the 3rd column.
@@ -11,7 +11,7 @@
 --		- Ensure all 'null' string values with an 'unknown' string value in the original `segment` column as well as the new `age_band` and `demographic` columns.
 --		- Generate a new `avg_transaction` column as the `sales` value divided by `transactions` rounded to 2 decimal places for each record.
 
-DROP TABLE IF EXISTS data_mart.dbo.cleaned_weekly_sales;
+DROP TABLE IF EXISTS data_mart.dbo.clean_weekly_sales;
 SELECT CONVERT(DATE, week_date, 3) AS week_date,
        DATEPART(ww, CONVERT(DATE, week_date, 3)) AS week_number,
        MONTH(CONVERT(DATE, week_date, 3)) AS month_number,
@@ -37,9 +37,9 @@ SELECT CONVERT(DATE, week_date, 3) AS week_date,
        transactions,
        CONVERT(BIGINT, sales) AS sales,
        CAST(ROUND(1.0 * sales / transactions, 2) AS REAL) AS avg_transaction 
-INTO data_mart.dbo.cleaned_weekly_sales
+INTO data_mart.dbo.clean_weekly_sales
 FROM data_mart.dbo.weekly_sales;
 
 SELECT * 
-FROM data_mart.dbo.cleaned_weekly_sales
+FROM data_mart.dbo.clean_weekly_sales
 ORDER BY week_date;
