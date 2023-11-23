78d0af5a0371a82d76b9b17bd33dd013204c60e4
khanhtkle <137462514+khanhtkle@users.noreply.github.com>
2023-11-23 10:31:38 +0700

    Upload MySQL Scripts

--- Case Study #5 - Data Mart/SQL Scripts/MySQL/mysql-5-Data-Mart-D-Bonus-Question.sql
+++ Case Study #5 - Data Mart/SQL Scripts/MySQL/mysql-5-Data-Mart-D-Bonus-Question.sql
@@ -2,11 +2,11 @@
 -- D. Bonus Question --
 -------------------------------------
 -- 	Which areas of the business have the highest negative impact in sales metrics performance in 2020 for the 12 week before and after period?
--- 	- region
--- 	- platform
--- 	- age_band
--- 	- demographic
--- 	- customer_type
+-- 		- region
+-- 		- platform
+-- 		- age_band
+-- 		- demographic
+-- 		- customer_type
 -- 	Do you have any further recommendations for Danny’s team at Data Mart or any interesting insights based off this analysis?
 
 DROP TABLE IF EXISTS data_mart.sales_variance;
@@ -32,7 +32,7 @@ SELECT *,
        DENSE_RANK() OVER (PARTITION BY region, platform
                           ORDER BY cml_sales_4wk_post - cml_sales_4wk_pre) AS variance_ranking,
        DENSE_RANK() OVER (PARTITION BY region, platform
-						  ORDER BY CAST(100.0 * (cml_sales_4wk_post - cml_sales_4wk_pre) / cml_sales_4wk_pre AS DECIMAL(5, 2))) AS variance_pct_ranking
+						  ORDER BY 100.0 * (cml_sales_4wk_post - cml_sales_4wk_pre) / cml_sales_4wk_pre) AS variance_pct_ranking
 FROM cumulative_sales_cte);
 
 SELECT *
