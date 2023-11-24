017ef2ef48ea07a52c008aea2c668e3338d520f1
khanhtkle <137462514+khanhtkle@users.noreply.github.com>
2023-11-24 09:07:34 +0700

    Upload MySQL Scripts

---
+++ Case Study #6 - Clique Bait/SQL Scripts/MySQL/mysql-6-Data-Mart-A-Digital-Analysis.sql
@@ -0,0 +1,134 @@
+-------------------------------------
+-- A. Digital Analysis --
+-------------------------------------
+-- 	1. How many users are there?
+
+SELECT COUNT(DISTINCT user_id) AS total_users
+FROM clique_bait.users;
+
+--  2. How many cookies does each user have on average?
+
+SELECT CAST(COUNT(cookie_id) / COUNT(DISTINCT user_id) AS DECIMAL(5,2)) AS avg_cookies_per_user
+FROM clique_bait.users;
+
+--  3. What is the unique number of visits by all users per month?
+
+SELECT YEAR(event_time) AS year,
+       MONTH(event_time) AS month_index,
+	   MONTHNAME(event_time) AS month,
+	   COUNT(DISTINCT visit_id) AS visits_per_month
+FROM clique_bait.events
+GROUP BY 1, 2, 3
+ORDER BY 1; 
+
+--  4. What is the number of events for each event type?
+
+SELECT ev.event_type,
+	   event_name,
+	   COUNT(ev.event_type) AS event_count
+FROM clique_bait.events AS ev
+JOIN clique_bait.event_identifier AS ei ON ei.event_type = ev.event_type
+GROUP BY 1 , 2
+ORDER BY 1;
+
+--  5. What is the percentage of visits which have a purchase event?
+
+WITH purchase_visit_cte AS 
+  (SELECT COUNT(DISTINCT visit_id) AS purchase_visit_count
+   FROM clique_bait.events
+   WHERE event_type = 3),
+	total_visits_cte AS 
+  (SELECT COUNT(DISTINCT visit_id) AS total_visits
+   FROM clique_bait.events)
+SELECT purchase_visit_count,
+	   total_visits,
+	   CAST(100 * purchase_visit_count / total_visits AS DECIMAL(5,2)) AS purchase_visit_pct
+FROM purchase_visit_cte, 
+     total_visits_cte;
+
+--  6. What is the percentage of visits which view the checkout page but do not have a purchase event (abandoned)?
+
+WITH next_page_id_cte AS
+  (SELECT visit_id,
+          ev.page_id,
+          page_name,
+          ev.event_type,
+          event_name,
+          event_time,
+          LEAD(ev.page_id) OVER (PARTITION BY visit_id
+                                 ORDER BY event_time, sequence_number) AS next_page_id
+   FROM clique_bait.events AS ev
+   JOIN clique_bait.event_identifier AS ei ON ei.event_type = ev.event_type
+   JOIN clique_bait.page_hierarchy AS ph ON ph.page_id = ev.page_id),
+     abandoned_visit_cte AS
+  (SELECT COUNT(DISTINCT visit_id) AS abandoned_visit_count
+   FROM next_page_id_cte
+   WHERE page_id = 12
+     AND next_page_id IS NULL),
+     checkout_visit_cte AS
+  (SELECT COUNT(DISTINCT visit_id) AS checkout_visit_count
+   FROM clique_bait.events
+   WHERE page_id = 12),
+     total_visits_cte AS
+  (SELECT COUNT(DISTINCT visit_id) AS total_visits
+   FROM clique_bait.events)
+SELECT abandoned_visit_count,
+       checkout_visit_count,
+       CAST(100.0 * abandoned_visit_count / checkout_visit_count AS DECIMAL(5,2)) AS abandoned_over_check_out_pct,
+       total_visits,
+       CAST(100.0 * abandoned_visit_count / total_visits AS DECIMAL(5,2)) AS abandoned_over_total_visits_pct
+FROM abandoned_visit_cte,
+     checkout_visit_cte,
+     total_visits_cte;
+
+--  7. What are the top 3 pages by number of views?
+
+SELECT ev.page_id, 
+	   page_name, 
+       COUNT(event_time) AS views
+FROM clique_bait.events AS ev
+JOIN clique_bait.event_identifier AS ei ON ei.event_type = ev.event_type
+JOIN clique_bait.page_hierarchy AS ph ON ph.page_id = ev.page_id
+WHERE ev.event_type = 1
+GROUP BY 1 , 2
+ORDER BY 3 DESC
+LIMIT 3;
+
+--  8. What is the number of views and cart adds for each product category?
+
+SELECT product_category,
+       SUM(CASE
+               WHEN ev.event_type = 1 THEN 1
+               ELSE 0
+           END) AS views,
+       SUM(CASE
+               WHEN ev.event_type = 2 THEN 1
+               ELSE 0
+           END) AS cart_adds
+FROM clique_bait.events AS ev
+JOIN clique_bait.page_hierarchy AS ph ON ph.page_id = ev.page_id
+WHERE product_category IS NOT NULL
+GROUP BY 1
+ORDER BY 1;
+
+--  9. What are the top 3 products by purchases?
+
+WITH final_page_id_cte AS
+  (SELECT product_id,
+          page_name AS product_name,
+          ev.event_type,
+          MAX(ev.page_id) OVER (PARTITION BY visit_id) AS final_page_id_from_this_visit_id
+   FROM clique_bait.events AS ev
+   JOIN clique_bait.event_identifier AS ei ON ei.event_type = ev.event_type
+   JOIN clique_bait.page_hierarchy AS ph ON ph.page_id = ev.page_id)
+SELECT product_id,
+       product_name,
+       SUM(CASE
+               WHEN event_type = 2
+                    AND final_page_id_from_this_visit_id = 13 THEN 1
+               ELSE 0
+           END) AS purchase_count
+FROM final_page_id_cte
+GROUP BY 1, 2
+ORDER BY 3 DESC
+LIMIT 3;
---
+++ Case Study #6 - Clique Bait/SQL Scripts/MySQL/mysql-6-Data-Mart-C-Campaigns-Analysis.sql
@@ -0,0 +1,178 @@
+-------------------------------------
+-- C. Campaigns Analysis --
+-------------------------------------
+-- 	Generate a table that has 1 single row for every unique `visit_id` record and has the following columns:
+-- 		- `user_id`
+-- 		- `visit_id`
+-- 		- `visit_start_time`: the earliest `event_time` for each visit
+-- 		- `page_views`: count of page views for each visit
+-- 		- `cart_adds`: count of product cart add events for each visit
+-- 		- `purchase`: 1/0 flag if a purchase event exists for each visit
+-- 		- `campaign_name`: map the visit to a campaign if the `visit_start_time` falls between the `start_date` and `end_date`
+-- 		- `impression`: count of ad impressions for each visit
+-- 		- `click`: count of ad clicks for each visit
+-- 		- `cart_products`: a comma separated text value with products added to the cart sorted by the order they were added to the cart
+
+DROP TABLE IF EXISTS clique_bait.campaign_analysis;
+CREATE TABLE clique_bait.campaign_analysis AS
+  (SELECT user_id,
+          ev.visit_id,
+          MIN(event_time) AS visit_start_time,
+          SUM(CASE
+                  WHEN ev.event_type = 1 THEN 1
+                  ELSE 0
+              END) AS page_views,
+          SUM(CASE
+                  WHEN ev.event_type = 2 THEN 1
+                  ELSE 0
+              END) AS cart_adds,
+          SUM(CASE
+                  WHEN ev.event_type = 3 THEN 1
+                  ELSE 0
+              END) AS purchase,
+          ci.campaign_name,
+          SUM(CASE
+                  WHEN ev.event_type = 4 THEN 1
+                  ELSE 0
+              END) AS impression,
+          SUM(CASE
+                  WHEN ev.event_type = 5 THEN 1
+                  ELSE 0
+              END) AS click,
+          GROUP_CONCAT(CASE
+                           WHEN ev.event_type = 2 THEN page_name
+                       END ORDER BY sequence_number SEPARATOR ', ') AS product_carts
+   FROM clique_bait.users AS us
+   LEFT JOIN clique_bait.events AS ev ON ev.cookie_id = us.cookie_id
+   LEFT JOIN clique_bait.event_identifier AS ei ON ei.event_type = ev.event_type
+   LEFT JOIN clique_bait.page_hierarchy AS ph ON ph.page_id = ev.page_id
+   LEFT JOIN clique_bait.campaign_identifier AS ci ON ev.event_time BETWEEN ci.start_date AND ci.end_date
+   GROUP BY 1, 2, 7
+   ORDER BY 1, 2, 3);
+
+SELECT *
+FROM clique_bait.campaign_analysis;
+
+-- 	Identifying users who have received impressions during each campaign period and comparing each metric with other users who did not have an impression event.
+
+WITH impression_stat_cte AS
+  (SELECT *,
+          CASE
+              WHEN SUM(impression) OVER (PARTITION BY user_id, campaign_name
+                                         ORDER BY visit_start_time 
+                                         ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) > 0 THEN 'Received impression'
+              ELSE 'No impression'
+          END AS impression_stat
+   FROM clique_bait.campaign_analysis)
+SELECT impression_stat,
+       COUNT(*) AS total_visits,
+       CAST(AVG(page_views) AS DECIMAL(5,2)) AS avg_page_view_per_visit,
+       CAST(AVG(cart_adds) AS DECIMAL(5,2)) AS avg_cart_add_per_visits,
+       CAST(AVG(purchase) AS DECIMAL(5,2)) AS avg_visit_to_purchase_conversion_rate
+FROM impression_stat_cte
+GROUP BY 1;
+
+-- 	Does clicking on an impression lead to higher purchase rates?
+
+WITH impression_stat_cte AS
+  (SELECT *,
+          CASE
+              WHEN SUM(impression) OVER (PARTITION BY user_id, campaign_name
+                                         ORDER BY visit_start_time 
+                                         ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) > 0
+                   AND SUM(click) OVER (PARTITION BY user_id, campaign_name
+                                        ORDER BY visit_start_time 
+                                        ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) > 0 THEN '1. Received + Clicking impression'
+              WHEN SUM(impression) OVER (PARTITION BY user_id, campaign_name
+                                         ORDER BY visit_start_time 
+                                         ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) > 0
+                   AND SUM(click) OVER (PARTITION BY user_id, campaign_name
+                                        ORDER BY visit_start_time 
+                                        ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) = 0 THEN '2. Received + No clicking impression'
+              ELSE '3. No impression'
+          END AS impression_stat
+   FROM clique_bait.campaign_analysis)
+SELECT impression_stat,
+       COUNT(*) AS total_visits,
+       CAST(AVG(page_views) AS DECIMAL(5,2)) AS avg_page_view_per_visit,
+       CAST(AVG(cart_adds) AS DECIMAL(5,2)) AS avg_cart_add_per_visit,
+       CAST(AVG(purchase) AS DECIMAL(5,2)) AS avg_visit_to_purchase_conversion_rate
+FROM impression_stat_cte
+GROUP BY 1
+ORDER BY 1;
+
+-- 	What is the uplift in purchase rate when comparing users who click on a campaign impression versus users who do not receive an impression? What if we compare them with users who have just an impression but do not click?
+
+WITH impression_stat_cte AS
+  (SELECT *,
+          CASE
+              WHEN SUM(impression) OVER (PARTITION BY user_id, campaign_name
+                                         ORDER BY visit_start_time 
+                                         ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) > 0
+                   AND SUM(click) OVER (PARTITION BY user_id, campaign_name
+                                        ORDER BY visit_start_time 
+                                        ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) > 0 THEN '1. Received + Clicking impression'
+              WHEN SUM(impression) OVER (PARTITION BY user_id, campaign_name
+                                         ORDER BY visit_start_time 
+                                         ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) > 0
+                   AND SUM(click) OVER (PARTITION BY user_id, campaign_name
+                                        ORDER BY visit_start_time 
+                                        ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) = 0 THEN '2. Received + No clicking impression'
+              ELSE '3. No impression'
+          END AS impression_stat
+   FROM clique_bait.campaign_analysis),
+     purchase_rate_cte AS
+  (SELECT impression_stat,
+          CAST(AVG(purchase) AS DECIMAL(5,2)) AS avg_purchase_rate
+   FROM impression_stat_cte
+   GROUP BY impression_stat)
+SELECT pr1.impression_stat,
+       pr2.avg_purchase_rate AS avg_purchase_rate_no_impression,
+       pr1.avg_purchase_rate AS avg_purchase_rate_received_impression,
+       pr1.avg_purchase_rate - pr2.avg_purchase_rate AS avg_purchase_rate_uplift
+FROM purchase_rate_cte AS pr1,
+     purchase_rate_cte AS pr2
+WHERE pr1.impression_stat != '3. No impression'
+  AND pr2.impression_stat = '3. No impression';
+
+-- 	What metrics can you use to quantify the success or failure of each campaign compared to each other?
+
+WITH impression_stat_cte AS
+  (SELECT *,
+          CASE
+              WHEN SUM(impression) OVER (PARTITION BY user_id, campaign_name
+                                         ORDER BY visit_start_time 
+                                         ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) > 0 THEN 'Received impression'
+              ELSE 'No impression'
+          END AS impression_stat
+   FROM clique_bait.campaign_analysis)
+SELECT campaign_name,
+       impression_stat,
+       COUNT(*) AS total_visits,
+       CAST(AVG(page_views) AS DECIMAL(5,2)) AS avg_page_view_per_visit,
+       CAST(AVG(cart_adds) AS DECIMAL(5,2)) AS avg_cart_add_per_visit,
+       CAST(AVG(purchase) AS DECIMAL(5,2)) AS avg_visit_to_purchase_conversion_rate
+FROM impression_stat_cte
+WHERE impression_stat = 'Received impression'
+GROUP BY 1, 2
+ORDER BY 1, 2 DESC;
+
+WITH impression_stat_cte AS
+  (SELECT *,
+          CASE
+              WHEN SUM(impression) OVER (PARTITION BY user_id, campaign_name
+                                         ORDER BY visit_start_time 
+                                         ROWS BETWEEN UNBOUNDED PRECEDING AND CURRENT ROW) > 0 THEN 'Received impression'
+              ELSE 'No impression'
+          END AS impression_stat
+   FROM clique_bait.campaign_analysis)
+SELECT campaign_name,
+       impression_stat,
+       COUNT(*) AS total_visits,
+       CAST(AVG(page_views) AS DECIMAL(5,2)) AS avg_page_view_per_visit,
+       CAST(AVG(cart_adds) AS DECIMAL(5,2)) AS avg_cart_add_per_visit,
+       CAST(AVG(purchase) AS DECIMAL(5,2)) AS avg_visit_to_purchase_conversion_rate
+FROM impression_stat_cte
+WHERE impression_stat = 'No impression'
+GROUP BY 1, 2
+ORDER BY 1, 2 DESC;
